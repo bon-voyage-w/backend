4e7725f1eaf86e3a21ad4a9c9fede9d1024571e8
김푸름 <prkim99@gmail.com>
2023-05-02 17:12:45 +0900

    [setting] gitignore 설정

---
+++ bonvoyageW/src/main/java/com/bonvoyage/main/BonvoyageWApplication.java
@@ -0,0 +1,13 @@
+package com.bonvoyage.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BonvoyageWApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BonvoyageWApplication.class, args);
+	}
+
+}
