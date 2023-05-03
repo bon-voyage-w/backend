734d1d5cf72aaf8e39aa90cdc721c4b086bdb095
오채영 <web3765@gmail.com>
2023-05-03 14:24:48 +0900

    [feat] attraction repository 생성

--- bonvoyageW/src/main/java/com/bonvoyage/attraction/entity/AttractionDescriptionEntity.java
+++ bonvoyageW/src/main/java/com/bonvoyage/attraction/entity/AttractionDescriptionEntity.java
@@ -1,4 +1,31 @@
 package com.bonvoyage.attraction.entity;
 
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "attraction_description")
 public class AttractionDescriptionEntity {
+
+    @Id
+    private int contentId;
+    private String homepage;
+    private String overview;
+    private String telname;
+
+    @Builder
+    public AttractionDescriptionEntity(int contentId, String homepage, String overview, String telname) {
+        this.contentId = contentId;
+        this.homepage = homepage;
+        this.overview = overview;
+        this.telname = telname;
+    }
 }
---
+++ bonvoyageW/src/test/java/com/attraction/AttractionTests.java
@@ -0,0 +1,8 @@
+package com.attraction;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AttractionTests {
+
+}
