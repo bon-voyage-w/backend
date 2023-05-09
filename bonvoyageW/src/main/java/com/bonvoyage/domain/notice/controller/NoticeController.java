5fb5352732c0c5b0eaf51689f082e325c705fb63
오채영 <web3765@gmail.com>
2023-05-09 17:44:29 +0900

    [feat] notice controller 추가

--- bonvoyageW/src/main/java/com/bonvoyage/domain/notice/controller/NoticeController.java
+++ bonvoyageW/src/main/java/com/bonvoyage/domain/notice/controller/NoticeController.java
@@ -9,34 +9,37 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/notice")
+@RequestMapping("/notices")
 @RequiredArgsConstructor
 public class NoticeController {
 
     private final NoticeService noticeService;
 
     @GetMapping("")
-    public String getNoticeList() {
-        return "";
+    public List<NoticeDto> getNoticeList() {
+        return noticeService.getNoticeList();
     }
 
     @PostMapping("")
-    public String registerNotice(){
+    public String registerNotice(@RequestBody NoticeDto noticeDto){
+        noticeService.addNotice(noticeDto);
         return "";
     }
 
     @GetMapping("/{noticeId}")
-    public String findByNoticeId(@PathVariable("noticeId") Long noticeId){
-        return "";
+    public NoticeDto findByNoticeId(@PathVariable("noticeId") Long noticeId){
+        return noticeService.findByNoticeId(noticeId);
     }
 
     @PutMapping("/{noticeId}")
-    public String modifyNotice(@PathVariable("noticeId") Long noticeId){
+    public String modifyNotice(@PathVariable("noticeId") Long noticeId, @RequestBody NoticeDto noticeDto){
+        noticeService.modifyNotice(noticeId, noticeDto);
         return "";
     }
 
     @DeleteMapping("/{noticeId}")
     public String deleteNotice(@PathVariable("noticeId") Long noticeId) {
+        noticeService.removeNotice(noticeId);
         return "";
     }
 
