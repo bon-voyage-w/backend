b1fd55faee0d3f956c0fb265db457a337a9cffb4
김푸름 <prkim99@gmail.com>
2023-05-19 15:59:31 +0900

    [feat] springdoc으로 변경

--- bonvoyageW/src/main/java/com/bonvoyage/config/SwaggerConfig.java
+++ bonvoyageW/src/main/java/com/bonvoyage/config/SwaggerConfig.java
@@ -1,33 +1,32 @@
 package com.bonvoyage.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-@EnableSwagger2
 @Configuration
-public class SwaggerConfig{
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("Bonvoyage Project API")
-                .description("Bonvoyage api")
-                .version("0.1")
-                .build();
-    }
+public class SwaggerConfig {
     @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .useDefaultResponseMessages(false)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.bonvoyage.domain"))
-                .paths(PathSelectors.any())
-                .build()
-                .apiInfo(apiInfo());
+    public OpenAPI openAPI() {
+        Info info = new Info().title("BonVoyage")
+                .description("BonVoyage API Reference for Developer")
+                .termsOfService("http://swagger.io/terms/")
+                .license(new License()
+                        .name("Apache License Version 2.0")
+                        .url("http://www.apache.org/licenses/LICENSE-2.0"));
+
+        return new OpenAPI()
+                .info(info);
+    }
+
+    @Bean//domain별로 작성하기!
+    public GroupedOpenApi user(){
+        return GroupedOpenApi.builder()
+                .group("user")
+                .pathsToMatch("/users/**")
+                .packagesToScan("com.bonvoyage.domain")
+                .build();
     }
 }
