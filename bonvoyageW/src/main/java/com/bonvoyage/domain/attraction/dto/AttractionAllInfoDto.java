102f49354b51d0efefc575c6488dac5826f23051
오채영 <web3765.gmail.com>
2023-05-20 13:51:49 +0900

    [feat] attracion allInfoDto 추가

---
+++ bonvoyageW/src/main/java/com/bonvoyage/domain/attraction/dto/AttractionAllInfoDto.java
@@ -0,0 +1,22 @@
+package com.bonvoyage.domain.attraction.dto;
+
+public class AttractionAllInfoDto {
+
+//    AttractionInfo
+    private long contentId;
+    private String title;
+    private String addr1;
+    private String tel;
+    private String firstImage;
+    private int sidoCode;
+    private int gugunCode;
+    private double latitude;
+    private double longitude;
+//    AttractionDescription
+    private String overview;
+
+//    AttractionDetail
+    private String cat1;
+    private String cat2;
+    private String cat3;
+}
