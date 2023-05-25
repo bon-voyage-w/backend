ee30f9501f71d5c7d532b66d988c57cdec0951aa
prkim99@gmail.com <prkim99@gmail.com>
2023-05-26 03:27:51 +0900

    [feat] 함수 이름 바꾸기

--- bonvoyageW/src/main/java/com/bonvoyage/domain/shareboard/controller/ShareBoardController.java
+++ bonvoyageW/src/main/java/com/bonvoyage/domain/shareboard/controller/ShareBoardController.java
@@ -27,7 +27,7 @@ public class ShareBoardController {
     @PostMapping()
     public ResponseEntity<?> shareBoardAdd(@RequestHeader("Authorization") String accessToken,
                                            @RequestBody ShareBoardDto shareBoardDto){
-        if(jwtService.checkToken(accessToken)){
+        if(jwtService.isUnavailToken(accessToken)){
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("인증되지 않은 사용자입니다");
         }
         int userId=jwtService.getUserId(accessToken);
@@ -38,7 +38,7 @@ public class ShareBoardController {
     @GetMapping("/{id}")
     public ResponseEntity<?> shareBoardDetail(@RequestHeader("Authorization") String accessToken,
                                               @PathVariable("id") int shareBoardId){
-        if(jwtService.checkToken(accessToken)){
+        if(jwtService.isUnavailToken(accessToken)){
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("인증되지 않은 사용자입니다");
         }
         ShareBoardDto shareBoardDto=shareBoardService.findShareBoardDetailById(shareBoardId);
@@ -47,7 +47,7 @@ public class ShareBoardController {
     @DeleteMapping("/{id}")
     public ResponseEntity<?> shareBoardRemove(@RequestHeader("Authorization") String accessToken,
                                               @PathVariable("id") int shareBoardId){
-        if(jwtService.checkToken(accessToken)){
+        if(jwtService.isUnavailToken(accessToken)){
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("인증되지 않은 사용자입니다");
         }
         int userId=jwtService.getUserId(accessToken);
@@ -64,7 +64,7 @@ public class ShareBoardController {
     public ResponseEntity<?> shareBoardModify(@RequestHeader("Authorization") String accessToken,
                                               @PathVariable("id") int shareBoardId,
                                               @RequestBody ShareBoardDto shareBoardDto){
-        if(jwtService.checkToken(accessToken)){
+        if(jwtService.isUnavailToken(accessToken)){
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("인증되지 않은 사용자입니다");
         }
         int userId=jwtService.getUserId(accessToken);
@@ -81,7 +81,7 @@ public class ShareBoardController {
     @PostMapping("/comments")
     public ResponseEntity<?> commentAdd(@RequestHeader("Authorization") String accessToken,
                                               @RequestBody ShareBoardCommentDto shareBoardCommentDto){
-        if(jwtService.checkToken(accessToken)){
+        if(jwtService.isUnavailToken(accessToken)){
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("인증되지 않은 사용자입니다");
         }
         int userId=jwtService.getUserId(accessToken);
@@ -93,7 +93,7 @@ public class ShareBoardController {
     @DeleteMapping("/comments/{id}")
     public ResponseEntity<?> commentRemove(@RequestHeader("Authorization") String accessToken,
                                               @PathVariable("id") int commentId){
-        if(jwtService.checkToken(accessToken)){
+        if(jwtService.isUnavailToken(accessToken)){
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("인증되지 않은 사용자입니다");
         }
         int userId=jwtService.getUserId(accessToken);
